a38089aa278dfa7171e83b6e417edac950f26380
Eder Sena <eder.sena@live.com>
2020-06-11 15:53:13 -0300

    pokemon api done

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import getAllPokemon from './services/getAllPokemon'
+import { getAllPokemon, getPokemon } from './services/getAllPokemon'
+import Card from './components/Card/'
+import Navbar from './components/Navbar/'
+
 import './App.css';
 
 function App() {
@@ -12,19 +15,70 @@ function App() {
   useEffect(() => {
     async function fetchData(){
       let response = await getAllPokemon(initialUrl);
-      console.log(response);
       setNextUrl(response.next);
       setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
       setLoading(false);
     }
     fetchData();
   }, [])
 
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results)
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!prevUrl){
+      setLoading(true);
+    }
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results)
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    }))
+
+    setPokemonData(_pokemonData)
+  }
+
+  console.log(pokemonData);
+
   return (
     <div>
-    { loading ? <h1>Loading...</h1> : (
-      <h1>Data is fetched!</h1>
-    )}
+    {
+      loading ? <h1>Loading...</h1> : (
+      <>
+      <Navbar />
+      <div className='btn'>
+        <button onClick={prev}>Prev</button>
+        <button onClick={next}>Next</button>
+      </div>
+        <div className="grid-container">
+          {pokemonData.map((pokemon, i) => {
+            return (
+              <Card key={i} pokemon={pokemon}/>
+            )
+          })}
+        </div>
+        <div className='btn'>
+        <button onClick={prev}>Prev</button>
+        <button onClick={next}>Next</button>
+      </div>
+      </>
+      )
+    }
     </div>
   );
 }
